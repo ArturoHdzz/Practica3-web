36ab537df3cdaca80a3660c68c1387113a216001
ArturoHdzz <hernandez020610@gmail.com>
2025-02-11 19:54:09 -0600

    mejoras

--- js/index.js
+++ js/index.js
@@ -1,32 +1,19 @@
 AOS.init();
-
-const animation_1 = () => {
-  document.querySelector("#intro_logo").classList.add("d-none");
-};
-
-const animation_2 = () => {
-  document.querySelector("#hero").classList.add("fade_in");
-  document.querySelector("#img_hero_1").classList.add("img_left_right");
-  document.querySelector("#img_hero_2").classList.add("img_right_left");
-};
-
-const animation_3 = () => {
-  document.querySelector("#span_icon").classList.remove("d-none");
-  document.querySelector("body").classList.remove("scroll-y-none");
-};
-
-const favicon = document.querySelector("#favicon");
-const logo = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
-  ? "assets/crown_logo_white.webp"
-  : "assets/crown_logo.webp";
-favicon.href = logo;
-
+const animation_1 = () => document.querySelector("#intro_logo").classList.add("d-none"),
+      animation_2 = () => {
+        document.querySelector("#hero").classList.add("fade_in");
+        document.querySelector("#img_hero_1").classList.add("img_left_right");
+        document.querySelector("#img_hero_2").classList.add("img_right_left");
+      },
+      animation_3 = () => {
+        document.querySelector("#span_icon").classList.remove("d-none");
+        document.querySelector("body").classList.remove("scroll-y-none");
+      };
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("body").classList.add("scroll-y-none");
   document.querySelector("#img_hero_1").classList.remove("img_left_right");
   document.querySelector("#img_hero_2").classList.remove("img_right_left");
-
-  setTimeout(animation_1);
-  setTimeout(animation_2);
-  setTimeout(animation_3);
+  setTimeout(animation_1, 0);
+  setTimeout(animation_2, 0);
+  setTimeout(animation_3, 0);
 });
